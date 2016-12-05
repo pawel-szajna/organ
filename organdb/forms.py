0daaa7593cabfec7ce3e389fb24054f5a7ba26ca
Paweł Szajna <szajnapawel@gmail.com>
2016-12-05 14:22:24 +0100

    Updated search form field to match model

--- organdb/forms.py
+++ organdb/forms.py
@@ -9,7 +9,7 @@ class SearchForm(forms.Form):
         ('elektryczna', 'elektryczna'),
     ]
 
-    city = forms.CharField(label='Miejscowość', min_length=3, max_length=70, required=False)
+    city = forms.CharField(label='Miejscowość', min_length=3, max_length=30, required=False)
     location = forms.CharField(label='Lokalizacja', min_length=3, max_length=70, required=False)
     keyboards = forms.IntegerField(label='Klawiatury', min_value=1, max_value=9, required=False)
     pedalboard = forms.ChoiceField(label='Pedał', choices=(('b', 'obojętnie'), ('y', 'tak'), ('n', 'nie')),
